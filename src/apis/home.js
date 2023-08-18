18d03d83a685140e28b8fe9f605a5d55f517a967
leizhuzheng <leizhuzheng@qq.com>
2023-08-19 00:29:44 +0800

    home-使用panel组件实现新鲜好物

--- src/apis/home.js
+++ src/apis/home.js
@@ -2,4 +2,11 @@ import httpInstance from "@/utils/http";
 
 
 // 获取-轮播图数据  GET/home/banner
-export const getBannerAPI = () => httpInstance({url:"/home/banner"})
+export const getBannerAPI = () => httpInstance({ url: "/home/banner" })
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => httpInstance({ url: '/home/new' })
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -15,11 +15,5 @@ import HomePanel from "./components/HomePanel.vue"
   <HomeNew />
   <HomeHot />
   <homeProduct />
-  <!-- 测试面板组件 -->
-  <HomePanel title="新鲜好物" subTitle="新鲜好物,好多商品">
-      <div>我是新鲜好物的插槽内容</div>
-  </HomePanel>
-  <HomePanel title="人气推荐" subTitle="人气推荐,好多商品">
-    <div>我是人气推荐的插槽内容</div>
-  </HomePanel>
+  
 </template>

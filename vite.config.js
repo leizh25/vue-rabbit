54c73c55499ccde8cd5e7aae86b2b5ed1eaa3699
leizhuzheng <leizhuzheng@qq.com>
2023-08-18 18:52:57 +0800

    完成Element主题色的定制

--- vite.config.js
+++ vite.config.js
@@ -17,13 +17,25 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      //1.配置ElementPlus采用sass样式配色系统
+      resolvers: [ElementPlusResolver({ importStyle: "sass" })],
     }),
   ],
   resolve: {
     //实际的路径转换  @ -> src
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  //2.自动导入定制化样式文件进行样式覆盖
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })

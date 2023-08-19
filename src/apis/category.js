95859caa3d789ec27ca5b31993d4b4e894375f1b
leizhuzheng <leizhuzheng@qq.com>
2023-08-19 14:22:24 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,4 +1,13 @@
 import httpInstance from "@/utils/http";
 
 // 获取-二级分类列表 GET   /category
-export const getCategoryAPI = (id) => httpInstance({url:"/category",params:{id}})
+export const getCategoryAPI = (id) => httpInstance({ url: "/category", params: { id } })
+
+
+/**
+ * @description: 获取二级分类列表数据
+ * @param {*} id 分类id 
+ * @return {*}
+ */
+
+export const getCategoryFilterAPI = (id) => httpInstance({ url: '/category/sub/filter', params: { id } })
--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,5 +1,16 @@
 <script setup>
-
+import { ref,onMounted } from 'vue';
+import { getCategoryFilterAPI } from "@/apis/category";
+import { useRoute } from "vue-router";
+//获取面包屑导航数据
+const route = useRoute()
+const categoryData = ref({})
+const getCategoryData = async () => {
+    let res = await getCategoryFilterAPI(route.params.id)
+    console.log('res: ', res);
+    categoryData.value = res.result
+}
+onMounted(() => getCategoryData())
 
 </script>
 
@@ -9,9 +20,8 @@
         <div class="bread-container">
             <el-breadcrumb separator=">">
                 <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-                <el-breadcrumb-item :to="{ path: '/' }">居家
-                </el-breadcrumb-item>
-                <el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
+                <el-breadcrumb-item :to="{ path: `/category/${categoryData.parentId}` }">{{ categoryData.parentName }}</el-breadcrumb-item>
+                <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
         <div class="sub-container">

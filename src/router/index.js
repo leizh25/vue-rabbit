aee030f8b58cd1516ff59176e2408fc8965fc560
leizhuzheng <leizhuzheng@qq.com>
2023-08-19 17:59:06 +0800

    二级分类-路由滚动行为优化

--- src/router/index.js
+++ src/router/index.js
@@ -6,29 +6,35 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   //path和component对应关系的位置
   routes: [
-   {
-    path:'/',
-    component:() => import("@/views/Layout/index.vue"),
-    children:[
-      {
-        path:"",//置空   当浏览器访问/时, 这个组件也会得到渲染
-        component:() => import("@/views/Home/index.vue")
-      },
-      {
-        path:"category/:id",
-        component:() => import("@/views/Category/index.vue"),
-      },
-      {
-        path:"category/sub/:id",
-        component:() => import("@/views/SubCategory/index.vue")
-      }
-    ]
-   },
-   {
-    path:"/login",
-    component:import("@/views/Login/index.vue")
-   }
-  ]
+    {
+      path: '/',
+      component: () => import("@/views/Layout/index.vue"),
+      children: [
+        {
+          path: "",//置空   当浏览器访问/时, 这个组件也会得到渲染
+          component: () => import("@/views/Home/index.vue")
+        },
+        {
+          path: "category/:id",
+          component: () => import("@/views/Category/index.vue"),
+        },
+        {
+          path: "category/sub/:id",
+          component: () => import("@/views/SubCategory/index.vue")
+        }
+      ]
+    },
+    {
+      path: "/login",
+      component: import("@/views/Login/index.vue")
+    }
+  ],
+  //路由行为定制
+  scrollBehavior() {
+    return {
+      top: 0
+    }
+  }
 })
 
 export default router

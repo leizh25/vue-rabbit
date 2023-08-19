31be180e13afa423a682ad669af58a6f81dfe49f
leizhuzheng <leizhuzheng@qq.com>
2023-08-19 23:59:22 +0800

    放大镜-实现大图显示以及优化

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -25,7 +25,11 @@ const { elementX, elementY, isOutside } = useMouseInElement(target)
 //3.控制滑块跟随鼠标移动 (监听elementX/Y变化, 一旦变化 重新设置left/top)
 const left = ref(0)
 const top = ref(0)
+const positionX = ref(0)
+const positionY = ref(0)
 watch([elementY, elementY], () => {
+    //如果鼠标没有移入到盒子, 直接不执行后面的逻辑
+    if(isOutside.value) return
     //有效范围内控制滑块距离
     //横向
     if (elementX.value > 100 && elementX.value < 300) {
@@ -41,6 +45,10 @@ watch([elementY, elementY], () => {
     if (elementX.value < 100) left.value = 0
     if (elementY.value > 300) top.value = 200
     if (elementY.value < 100) top.value = 0
+
+    //控制大图的展示
+    positionX.value = - left.value * 2
+    positionY.value = - top.value * 2
 })
 </script>
 
@@ -51,7 +59,7 @@ watch([elementY, elementY], () => {
         <div class="middle" ref="target">
             <img :src="imageList[activeIndex]" alt="" />
             <!-- 蒙层小滑块 -->
-            <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
+            <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
         </div>
         <!-- 小图列表 -->
         <ul class="small">
@@ -62,11 +70,11 @@ watch([elementY, elementY], () => {
         <!-- 放大镜大图 -->
         <div class="large" :style="[
             {
-                backgroundImage: `url(${imageList[0]})`,
-                backgroundPositionX: `0px`,
-                backgroundPositionY: `0px`,
+                backgroundImage: `url(${imageList[activeIndex]})`,
+                backgroundPositionX: `${positionX}px`,
+                backgroundPositionY: `${positionY}px`,
             },
-        ]" v-show="false"></div>
+        ]" v-show="!isOutside"></div>
     </div>
 </template>
 

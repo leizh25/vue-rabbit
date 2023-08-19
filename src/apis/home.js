808acbe7aec4f9f409bf26555bdf6d105a22cc37
leizhuzheng <leizhuzheng@qq.com>
2023-08-19 10:33:31 +0800

    home-product列表渲染以及图片懒加载

--- src/apis/home.js
+++ src/apis/home.js
@@ -16,5 +16,13 @@ export const findNewAPI = () => httpInstance({ url: '/home/new' })
  * @param {*}
  * @return {*}
  */
-export const getHotAPI = () => httpInstance({url:'home/hot'})
+export const getHotAPI = () => httpInstance({ url: 'home/hot' })
+
+/**
+ * @description: 获取所有商品模块
+ * @param {*}
+ * @return {*}
+ */
+export const getGoodsAPI = () => httpInstance({ url: '/home/goods' })
+
 
--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -18,7 +18,7 @@ onMounted(() => getNewList())
         <ul class="goods-list">
             <li v-for="item in newList" :key="item.id">
                 <RouterLink to="/">
-                    <img v-img-lazy="item.picture" :src="item.picture" alt="" />
+                    <img v-img-lazy="item.picture" alt="" />
                     <p class="name">{{ item.name }}</p>
                     <p class="price">&yen;{{ item.price }}</p>
                 </RouterLink>

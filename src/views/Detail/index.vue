07860034bff14089c07756c1272c2b36308fb3b2
leizhuzheng <leizhuzheng@qq.com>
2023-08-20 20:32:28 +0800

    本地购物车-添加购物车

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -4,6 +4,8 @@ import XtxSku from "@/components/XtxSku/index.vue"
 import { ref, onMounted } from 'vue';
 import { getDetail } from '@/apis/detail';
 import { useRoute } from "vue-router";
+import { ElMessage } from "element-plus";
+import { useCartStore } from "@/stores/cartStore";
 const route = useRoute()
 const goods = ref({})
 const getGoods = async (id = route.params.id) => {
@@ -12,9 +14,39 @@ const getGoods = async (id = route.params.id) => {
     goods.value = res.result
 }
 onMounted(() => getGoods())
+
 //sku规格被操作时
-const skuChange = (obj) => {
-    console.log(obj);
+let skuObj = {}
+const skuChange = (sku) => {
+    console.log(sku);
+    skuObj = sku
+}
+
+//count
+const count = ref(1)
+const countChange = (count) => {
+    console.log('count: ', count);
+}
+
+//添加购物车
+const cartStore = useCartStore()
+const addCart = () => {
+if(skuObj.skuId){
+    //规格已经选择  触发action
+    cartStore.addCart({
+        id:goods.value.id,
+        name:goods.value.name,
+        picture:goods.value.mainPictures[0],
+        price:goods.value.price,
+        count:count.value,
+        skuId:skuObj.skuId,
+        attrsText:skuObj.specsText,
+        selected:true
+    })
+}else{
+    //规格没有选择  提示用户
+    ElMessage.warning("请选择规格")
+}
 }
 </script>
 
@@ -93,12 +125,10 @@ const skuChange = (obj) => {
                             <!-- sku组件 -->
                             <XtxSku :goods="goods" @change="skuChange"></XtxSku>
                             <!-- 数据组件 -->
-
+                            <el-input-number v-model="count" @change="countChange" />
                             <!-- 按钮组件 -->
                             <div>
-                                <el-button size="large" class="btn">
-                                    加入购物车
-                                </el-button>
+                                <el-button size="large" class="btn" @click="addCart">加入购物车</el-button>
                             </div>
 
                         </div>
